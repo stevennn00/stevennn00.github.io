9d89e589740442505f818a15562cb7582c925e59
Steven Whitehurst <sdwdab@gmail.com>
2020-05-10 17:27:37 -0400

    init
    added portfolio

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,21 +1,21 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
-"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
+  "assets/AssetManifest.json": "053476db3e77c181887eed62aab7c3af",
+"assets/assets/fonts/OpenSans.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+"assets/assets/fonts/Rajdhani-Light.ttf": "d81f6a473fc85da9e69f0737619596f0",
+"assets/assets/images/background.jpg": "74b32cccbc9601dbae6187f7ef87d9eb",
+"assets/FontManifest.json": "0b1e7cd7d0ae079c50932a8c9fe5c619",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "ff18b30a1f3d3e66171783acf6b2916c",
+"assets/LICENSE": "08953c8a21a2e323ce521afa8045ddc0",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "8dd4b48570b04cca29abedba6d5cdf1b",
-"icons/Icon-512.png": "8dd4b48570b04cca29abedba6d5cdf1b",
-"index.html": "3a7d8249feb10b93c2e3ca6e4945a39d",
-"/": "3a7d8249feb10b93c2e3ca6e4945a39d",
-"main.dart.js": "dc21a692017d1215a5f49d33bfa4bd12",
-"manifest.json": "d7efcb825f78e7aeac4a9c96b5479595"
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "92acaac4edce9810ac1c45deeba5b2b2",
+"/": "92acaac4edce9810ac1c45deeba5b2b2",
+"main.dart.js": "488b62992c771129aa67bef5c2426bd9",
+"manifest.json": "1e015b9b05e42bfbd2ae90f449fbe71f"
 };
 
 self.addEventListener('activate', function (event) {
